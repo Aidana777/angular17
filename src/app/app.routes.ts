0c232b3efa5a3b0486b6dc4327ce036b8e898cba
Aidana777 <aidananiazalieva27@gmail.com>
2023-12-29 21:52:23 +0600

    card create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { CardsComponent } from './cards/cards.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
 
 export const routes: Routes = [
-    {'path':'',component: CardsComponent},
-    { 'path': 'shopping-card', component: ShoppingCardComponent }
+    { 'path': '', component: CardsComponent },
+    { 'path': 'shopping-card', component: ShoppingCardComponent },
+    { 'path': '**', component: PageNotFoundComponent }
+
 
 ];
