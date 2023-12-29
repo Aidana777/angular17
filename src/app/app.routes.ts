20fc13d1191089a75466d98149ab868f96fe7a29
Aidana777 <aidananiazalieva27@gmail.com>
2023-12-29 18:35:15 +0600

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'',component: CardsComponent},
+    { 'path': 'shopping-card', component: ShoppingCardComponent }
+
+];
