9345ac62f5240793e06c68a378f71a921410c2f9
Aidana777 <aidananiazalieva27@gmail.com>
2024-04-03 11:05:54 +0600

    pagination add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,LayoutComponent,FormsModule, RouterOutlet,HttpClientModule  ],
+  imports: [CommonModule,LayoutComponent,FormsModule, RouterOutlet,HttpClientModule ,NgxPaginationModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule  } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CardService } from '../services/card.service';
@@ -12,9 +12,12 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
-export class CardsComponent {
+export class CardsComponent  {
   products: any[] = [];
   filteredProducts: any[] = [];
+  pagedProducts: any[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 8; // Укажите количество элементов на странице по своему усмотрению
 
   constructor(private httpClient: HttpClient, private cartService: CardService, private searchService: SearchService) { }
 
@@ -23,10 +26,12 @@ export class CardsComponent {
       .subscribe(data => {
         this.products = data;
         this.filteredProducts = this.products;
+        this.setPage(1); // Инициализация пагинации
       });
 
     this.searchService.searchQuery$.subscribe(query => {
       this.filteredProducts = this.filterProducts(query);
+      this.setPage(1); // Сброс пагинации при изменении поискового запроса
     });
   }
 
@@ -35,6 +40,21 @@ export class CardsComponent {
   }
 
   addToCart(product: any) {
-    this.cartService.addToCart(product); 
+    this.cartService.addToCart(product);
+  }
+
+  setPage(page: number) {
+    this.currentPage = page;
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = Math.min(startIndex + this.itemsPerPage, this.filteredProducts.length);
+    this.pagedProducts = this.filteredProducts.slice(startIndex, endIndex);
+  }
+
+  pageChanged(page: number) {
+    this.setPage(page);
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.filteredProducts.length / this.itemsPerPage);
   }
 }
